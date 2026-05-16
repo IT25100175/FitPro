8659ddc2948fb2fac03b9a9a4e08c865f0c2d8f2
IT25101159 <it25101159@my.sliit.lk>
2026-05-16 11:10:53 +0530

    add Login Request

---
+++ src/main/java/com/fitpro/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package com.fitpro.dto;
+public class LoginRequest {
+    private String userName;
+    private String password;
+    public String getUserName() { return userName; } public void setUserName(String v) { userName=v; }
+    public String getPassword() { return password; } public void setPassword(String v) { password=v; }
+}
